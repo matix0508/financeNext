eab5675bc49145142f08947d0463af44ec3531ad
Mateusz Wlekliński <wleklinskimateusz05@gmail.com>
2022-03-24 17:13:28 +0100

    init plot category

---
+++ components/categories/CategoryPlot.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { useQuery } from 'react-query';
+import {MyLineChart} from '../common/MyLineChart';
+
+export const CategoryPlot:FC = () => {
+    const {isLoading, error, data: rawData} = useQuery("/api/summary/2022")
+  return (
+    <MyLineChart data={} />
+  )
+}
